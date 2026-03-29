0c7b27d70bf3e6a5460aac25d6a25f963525d478
Marcel Owhonda <owhondamarcelprince@gmail.com>
2026-03-29 16:09:31 -0700

    feat: add reusable Cloud Run Terraform module and dev environment

--- terraform/environments/dev/cloud-run/variables.tf
+++ terraform/environments/dev/cloud-run/variables.tf
@@ -1,73 +1,78 @@
-variable "service_name" {
-  description = "Name of the Cloud Run service"
+variable "project_id" {
+  description = "GCP project ID"
   type        = string
 }
 
 variable "region" {
-  description = "Deployment region"
+  description = "GCP region"
+  type        = string
+}
+
+variable "service_name" {
+  description = "Cloud Run service name"
   type        = string
 }
 
 variable "image" {
-  description = "Container image to deploy"
+  description = "Container image"
   type        = string
 }
 
 variable "service_account_email" {
-  description = "Service account email for the Cloud Run service"
+  description = "Service account email for Cloud Run"
   type        = string
 }
 
 variable "container_port" {
-  description = "Container listening port"
+  description = "Application port"
   type        = number
   default     = 8080
 }
 
 variable "min_instance_count" {
-  description = "Minimum number of instances"
+  description = "Minimum instances"
   type        = number
   default     = 0
 }
 
 variable "max_instance_count" {
-  description = "Maximum number of instances"
+  description = "Maximum instances"
   type        = number
   default     = 2
 }
 
 variable "cpu" {
-  description = "CPU limit for the container"
+  description = "CPU limit"
   type        = string
   default     = "1"
 }
 
 variable "memory" {
-  description = "Memory limit for the container"
+  description = "Memory limit"
   type        = string
   default     = "512Mi"
 }
 
 variable "ingress" {
-  description = "Ingress setting for Cloud Run"
+  description = "Ingress policy"
   type        = string
   default     = "INGRESS_TRAFFIC_ALL"
 }
 
 variable "allow_unauthenticated" {
-  description = "Whether to allow unauthenticated access"
+  description = "Allow public access"
   type        = bool
   default     = false
 }
 
 variable "env_vars" {
-  description = "Environment variables for the container"
+  description = "Environment variables"
   type        = map(string)
   default     = {}
 }
 
 variable "labels" {
-  description = "Labels to apply to the service"
+  description = "Labels"
   type        = map(string)
   default     = {}
 }
---
+++ terraform/modules/cloud-run/outputs.tf
@@ -0,0 +1,14 @@
+output "service_name" {
+  description = "Cloud Run service name"
+  value       = google_cloud_run_v2_service.this.name
+}
+
+output "service_uri" {
+  description = "Cloud Run service URI"
+  value       = google_cloud_run_v2_service.this.uri
+}
+
+output "service_location" {
+  description = "Cloud Run service region"
+  value       = google_cloud_run_v2_service.this.location
+}
