99ab01bc93f232b35db875a9149cf9a8cc6584d0
Marcel Owhonda <owhondamarcelprince@gmail.com>
2026-03-29 08:56:41 -0700

    test: add insecure bucket to validate policy gates

--- terraform/insecure-example.tf
+++ terraform/insecure-example.tf
@@ -1,6 +1,12 @@
 resource "google_storage_bucket" "bad_bucket" {
-  name     = "paved-road-insecure-example-bucket"
-  location = "US"
+name = "paved-road-insecure-example-bucket-12345"
+location = "US"
 
-  uniform_bucket_level_access = false
+uniform_bucket_level_access = false
+}
+
+resource "google_storage_bucket_iam_member" "public_access" {
+bucket = google_storage_bucket.bad_bucket.name
+role = "roles/storage.objectViewer"
+member = "allUsers"
 }
