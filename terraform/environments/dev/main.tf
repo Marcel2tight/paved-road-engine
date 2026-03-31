6d28938d458950dc5b3d8170a03e04f884c23ee7
Marcel Owhonda <owhondamarcelprince@gmail.com>
2026-03-30 18:37:45 -0700

    cleanup: remove obsolete dev root terraform entrypoint

--- terraform/environments/dev/main.tf
+++
@@ -1,7 +0,0 @@
-module "cloud_run_service" {
-  source = "../../modules/cloud-run"
-
-  name   = "paved-road-dev-service"
-  image  = "us-docker.pkg.dev/cloudrun/container/hello"
-  region = "us-central1"
-}
