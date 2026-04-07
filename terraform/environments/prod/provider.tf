9f92710e253218b23aef3b14f54b43042301ace2
Marcel2tight <159199520+Marcel2tight@users.noreply.github.com>
2026-04-07 15:40:42 -0700

    feat: standardize stage and prod to environment-root deployment model (#10)

---
+++ terraform/environments/prod/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "imposing-fx-413205"
+  region  = "us-central1"
+}
